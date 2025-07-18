907647dda8130bb3e1ccbd274f694a579b05008d
besirkaraoglu <50145106+5karaoglu@users.noreply.github.com>
2025-07-18 17:54:17 +0300

    Configure API URL for production and development

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001/api',
 });
 
 // Interceptor to add the token to every request
